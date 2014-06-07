52aaaf6c5e07002b0008941d7ab27bdac02730a3
Markus Ullmark <ullmark@gmail.com>
2014-06-07 18:02:59 +0200

    swapped to gulp

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp      = require('gulp');
+var gutil     = require('gulp-util');
+var uglify    = require('gulp-uglify');
+var jshint    = require('gulp-jshint');
+var plumber   = require('gulp-plumber');
+var rename    = require('gulp-rename');
+
+// jshint
+// ---------------------
+
+gulp.task('jshint', function() {
+
+  gulp
+    .src('fluent-time.js')
+    .pipe(plumber())
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+
+});
+
+// scripts
+// ----------------------
+
+gulp.task('scripts', function() {
+
+  gulp
+    .src('fluent-time.js')
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./'));
+
+});
+
+gulp.task('watch', function() {
+});
+
+gulp.task('default', ['scripts']);
